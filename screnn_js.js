b7fb6c96953f9e435dfdcc661cb336164255dbe7
Wittgensteen <35298621+Wittgensteen@users.noreply.github.com>
2018-08-09 15:15:30 +0300

    Create screnn_js.js

---
+++ screnn_js.js
@@ -0,0 +1,22 @@
+function genScreenshot() {
+    html2canvas(document.body, {
+      onrendered: function(canvas) {
+      $('#capture').html("");
+			$('#capture').append(canvas);
+      
+      if (navigator.userAgent.indexOf("MSIE ") > 0 || 
+					navigator.userAgent.match(/Trident.*rv\:11\./)) 
+			{
+      	var blob = canvas.msToBlob();
+        window.navigator.msSaveBlob(blob,'Test file.png');
+      }
+      else {
+        $('#test').attr('href', canvas.toDataURL("image/png"));
+        $('#test').attr('download','Test file.png');
+        $('#test')[0].click();
+      }
+      
+      
+      }
+    });
+}
