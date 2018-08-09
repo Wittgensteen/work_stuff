ffed33ec56c44df91df9f3ccf57fe53e8ae4b7d9
Wittgensteen <35298621+Wittgensteen@users.noreply.github.com>
2018-08-09 15:37:14 +0300

    Update and rename screnn_js.js to screnn_js_nocan.js

--- screnn_js_nocan.js
+++ screnn_js_nocan.js
@@ -1,9 +1,7 @@
 function genScreenshot() {
     html2canvas(document.body, {
       onrendered: function(canvas) {
-      $('#capture').html("");
-			$('#capture').append(canvas);
-      
+      $('#capture').html("");      
       if (navigator.userAgent.indexOf("MSIE ") > 0 || 
 					navigator.userAgent.match(/Trident.*rv\:11\./)) 
 			{
